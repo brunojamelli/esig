3dd189182cdc617054f6e85dda3cf199d9af823e
brunojamelli <brunojamelli@ufrn.edu.br>
2021-12-04 18:20:28 -0300

    adição do recurso de exclusão e busca por status

--- src/main/java/com/jamelli/esigtask/api/TaskAPI.java
+++ src/main/java/com/jamelli/esigtask/api/TaskAPI.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,8 +31,21 @@ public class TaskAPI {
     }
 
     @GetMapping
-    public ResponseEntity<List<Task>> findAll() {
+    public ResponseEntity<List<Task>> getAll() {
         List<Task> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping(value = "/{status}")
+    public ResponseEntity<List<Task>> getAllByStatus(@PathVariable Boolean status) {
+        List<Task> list = service.findByStatus(status);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @DeleteMapping(value="/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
